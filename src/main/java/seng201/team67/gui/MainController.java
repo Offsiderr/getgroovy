80dfae1f73350d80edd9e0dd7c4f885f965b9b24
kau23 <kau23@uclive.ac.nz>
2026-04-07 12:06:01 +1200

    Added blinking cursor for the label name

--- src/main/java/seng201/team67/gui/MainController.java
+++ src/main/java/seng201/team67/gui/MainController.java
@@ -4,11 +4,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import javafx.util.Duration;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
 
-/**
- * Controller for the main.fxml window
- * @author seng201 teaching team
- */
 public class MainController {
 
     @FXML
@@ -17,19 +18,42 @@ public class MainController {
     @FXML
     private Button defaultButton;
 
-    /**
-     * Initialize the window
-     *
-     * @param stage Top level container for this window
-     */
+    @FXML
+    private Label nameDisplay;
+
+    private String guildName = "";
+    private boolean cursorVisible = true;
+    private Timeline cursorBlink;
+
     public void init(Stage stage) {
+        cursorBlink = new Timeline(
+                new KeyFrame(Duration.millis(500), e -> {
+                    cursorVisible = !cursorVisible;
+                    updateDisplay();
+                })
+        );
+        cursorBlink.setCycleCount(Timeline.INDEFINITE);
+        cursorBlink.play();
     }
 
-    /**
-     * Method to call when our counter button is clicked
-     *
-     */
+    private void updateDisplay() {
+        nameDisplay.setText(cursorVisible ? guildName + "_" : guildName);
+    }
+
+    @FXML
+    private void handleKeyPress(KeyEvent e) {
+        if (e.getCode() == KeyCode.BACK_SPACE) {
+            if (!guildName.isEmpty())
+                guildName = guildName.substring(0, guildName.length() - 1);
+        } else if (e.getText() != null && !e.getText().isEmpty()) {
+            if (guildName.length() < 15)
+                guildName += e.getText().toUpperCase();
+        }
+        updateDisplay();
+    }
+
     @FXML
     public void onButtonClicked() {
     }
+
 }
