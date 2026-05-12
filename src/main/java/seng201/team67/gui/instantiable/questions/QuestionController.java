84856e71c6983248c327b902004c2617e024c0ec
kenny <kau23@uclive.ac.nz>
2026-05-12 21:12:45 +1200

    Started working on the final UI for the main game

--- src/main/java/seng201/team67/gui/instantiable/questions/QuestionController.java
+++ src/main/java/seng201/team67/gui/instantiable/questions/QuestionController.java
@@ -3,6 +3,8 @@ package seng201.team67.gui.instantiable.questions;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import seng201.team67.models.questionmodels.Answer;
 import seng201.team67.models.questionmodels.Question;
 
@@ -54,12 +56,18 @@ public class QuestionController {
                 Answer choice = answers.get(i);
                 btn.setText(choice.getLabel());
                 btn.setVisible(true);
+                btn.setManaged(true);
                 btn.setOnAction(e -> onAnswer.accept(choice));
             } else {
                 btn.setVisible(false);
-                btn.setManaged(false);  // collapses layout space too
+                btn.setManaged(false);
             }
         }
+
+        GridPane grid = (GridPane) answerOne.getParent();
+        List<RowConstraints> rows = grid.getRowConstraints();
+        boolean row1Used = answers.size() > 2;
+        rows.get(1).setPercentHeight(row1Used ? 50.0 : 0.0);
+        rows.get(1).setMaxHeight(row1Used ? Double.MAX_VALUE : 0.0);
     }
 }
-
